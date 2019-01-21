bb02155c6ec2359d011eedbebf63e3baa2843b9f
Jorge Garduño <damnyorch@gmail.com>
2019-01-21 17:08:27 -0600

    boilerplate

--- src/utils/forceSlash.js
+++ src/utils/forceSlash.js
@@ -1,3 +1,8 @@
+/**
+ * @description fixes the path url, some cases it just dont prepend the slash
+ * @param expressApp
+ * @returns {function(*=, *=): *}
+ */
 export default expressApp => (request, response) => {
   if (!request.path) {
     request.url = `/${request.url}` // prepend '/' to keep query params if any
