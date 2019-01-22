045da3c6e33c7d287a0282717a1ce14c70702609
Jorge Garduño <damnyorch@gmail.com>
2019-01-22 11:37:02 -0600

    new dist

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -9,37 +7,23 @@ exports.default = void 0;
 
 var _triggers = require("./triggers");
 
-var _forceSlash = _interopRequireDefault(require("./utils/forceSlash"));
-
-var _ensureCors = _interopRequireDefault(require("./utils/ensureCors"));
-
 /**
  * @description source generator of the triggers
  */
-
-/**
- * @description HOC that fixes path errors for express/https functions
- */
-
-/**
- * @description HOC that ensure CORS for https functions
- */
-var _default = (firebase, functions) => {
+var _default = firebase => {
   return {
-    CheckDocument: functions.https.onRequest((0, _forceSlash.default)((0, _ensureCors.default)((0, _triggers.checkDocument)(firebase)))),
-    ImproveImage: functions.storage.object().onFinalize((0, _triggers.improveImage)(firebase))
+    /**
+     * @description https function that validates documents already improved
+     * @type {Function}
+     */
+    CheckDocument: (0, _triggers.checkDocument)(firebase),
+
+    /**
+     * @description Storage function that improves images for better Vision results
+     * @type {Function}
+     */
+    ImproveImage: (0, _triggers.improveImage)(firebase)
   };
-}; // export const RequestRegister = functions.https.onRequest(forceSlash(ensureCors(Triggers.requestRegister)))
-
-/**
- * @description https function that validates documents already improved
- * @type {HttpsFunction}
- */
-
-/**
- * @description Storage function that improves images for better Vision results
- * @type {CloudFunction<ObjectMetadata>}
- */
-
+};
 
 exports.default = _default;
