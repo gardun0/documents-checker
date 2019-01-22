b98ca2ca5c9f73964e76bd65b632b2fa00602094
Jorge Garduño <damnyorch@gmail.com>
2019-01-22 11:53:56 -0600

    enable functionallity for improveImage

--- src/triggers/improveimage.js
+++ src/triggers/improveimage.js
@@ -40,67 +40,67 @@ export default firebase => async object => {
   const { name, bucket, contentType } = object
 
   try {
-    console.log(object)
-    // /**
-    //  * @description instantiate the storage bucket
-    //  * @type {Storage.Bucket}
-    //  */
-    // const storage = firebase.storage().bucket(bucket)
-    //
-    // /**
-    //  * @description path from file
-    //  * @type {string}
-    //  */
-    // const path = dirname(name)
-    // console.log(path, name)
-    // if (!(contentType || mime.lookup(name)).includes('image/')) return null
-    //
-    // /**
-    //  * @description name of the file handled
-    //  * @type {string}
-    //  */
-    // const fileName = basename(name, extname(name))
-    //
-    // /**
-    //  * @description temp path for image
-    //  * @type {string}
-    //  */
-    // const tempPath = resolve(tmpdir(), name)
-    //
-    // /**
-    //  * @description upload path for image
-    //  * @type {string}
-    //  */
-    // const uploadPath = normalize(format({
-    //   ext: mime.extension(IMAGE_TYPE),
-    //   base: fileName,
-    //   dir: path
-    // }))
-    //
-    // /**
-    //  * @description temp path for converted image
-    //  * @type {string}
-    //  */
-    // const tempConvertedPath = join(tmpdir(), uploadPath)
-    //
-    // await mkdirp(basename(tempPath))
-    //
-    // /**
-    //  * @description downloads image to convert on temp directory
-    //  */
-    // await storage.file(name).download({ destination: tempPath })
-    //
-    // /**
-    //  * @description improves the image and create its into temp path
-    //  */
-    // await grayAndConvert(tempPath, tempConvertedPath)
-    //
-    // await storage.upload(tempConvertedPath, { destination: uploadPath })
-    //
-    // unlinkSync(tempConvertedPath)
-    // unlinkSync(tempPath)
-    //
-    // return null
+    /**
+     * @description instantiate the storage bucket
+     * @type {Storage.Bucket}
+     */
+    const storage = firebase.storage().bucket(bucket)
+
+    /**
+     * @description path from file
+     * @type {string}
+     */
+    const path = dirname(name)
+
+    if (!(contentType || mime.lookup(name)).includes('image/')) return null
+    if (!name.includes('waiting')) return null
+
+    /**
+     * @description name of the file handled
+     * @type {string}
+     */
+    const fileName = basename(name, extname(name))
+
+    /**
+     * @description temp path for image
+     * @type {string}
+     */
+    const tempPath = resolve(tmpdir(), name)
+
+    /**
+     * @description upload path for image
+     * @type {string}
+     */
+    const uploadPath = normalize(format({
+      ext: mime.extension(IMAGE_TYPE),
+      base: fileName,
+      dir: path
+    }))
+
+    /**
+     * @description temp path for converted image
+     * @type {string}
+     */
+    const tempConvertedPath = join(tmpdir(), uploadPath)
+
+    await mkdirp(basename(tempPath))
+
+    /**
+     * @description downloads image to convert on temp directory
+     */
+    await storage.file(name).download({ destination: tempPath })
+
+    /**
+     * @description improves the image and create its into temp path
+     */
+    await grayAndConvert(tempPath, tempConvertedPath)
+
+    await storage.upload(tempConvertedPath, { destination: uploadPath })
+
+    unlinkSync(tempConvertedPath)
+    unlinkSync(tempPath)
+
+    return null
   } catch (err) {
     console.error(err)
   }
